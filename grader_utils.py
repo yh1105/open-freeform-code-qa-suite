6e711b2350cddd46d4c98bc645418641130cebb7
Linyi Li <linyi.li@bytedance.com>
2023-11-08 01:36:27 -0800

    fix a vital bug for customized handler

--- grader_utils.py
+++ grader_utils.py
@@ -147,7 +147,7 @@ def blank_filling_match(template: str, blank_str: str, escape: str, targets: Lis
         func_name = post_handler['func']
 
         custom_module = importlib.import_module(module_name)
-        new_ans, new_tot, post_handler_detail = custom_module.__call__(func_name)(now_ans, now_tot, grading_details)
+        new_ans, new_tot, post_handler_detail = getattr(custom_module, func_name)(now_ans, now_tot, grading_details)
         now_ans = new_ans
         now_tot = new_tot
 
