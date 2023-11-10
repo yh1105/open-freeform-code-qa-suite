e73d3d2a8dce066038bb651cd0553c0a5dcfd003
ddaa2000 <2201111637@pku.edu.cn>
2023-11-10 16:27:28 +0800

    add custom py

---
+++ cases/test_334/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static class Subject{
+        String sub;
+        Integer marks;
+        boolean status;
+        // getters & setters
+    }
+    public static void removeFalseSubjects(List<Subject> subjects){
+        [blank]
+    }
+}
